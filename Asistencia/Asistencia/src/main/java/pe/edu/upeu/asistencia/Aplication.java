d430176f7e611e73ccb3331bdc99d8e422a8309b
AntoniApaza <antoniapaza64@gmail.com>
2025-08-21 10:27:55 -0500

    ron

---
+++ Asistencia/Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,10 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("Iniciamos suii Ron");
+        AsistenciaApplication.main(args);//llamamos a la clase aplication
+
+
+    }
+}
---
+++ Asistencia/src/test/java/pe/udu/upeu/asistencia/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.udu.upeu.asistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
