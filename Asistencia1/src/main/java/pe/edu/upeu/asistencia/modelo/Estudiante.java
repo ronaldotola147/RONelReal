cb5cddc661657e1e66dd2532c1951fcb74aeb59c
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-08-28 11:58:06 -0500

    WARONNNN

--- Asistencia1/src/main/java/pe/edu/upeu/asistencia/modelo/Estudiante.java
+++ Asistencia1/src/main/java/pe/edu/upeu/asistencia/modelo/Estudiante.java
@@ -3,8 +3,12 @@ package pe.edu.upeu.asistencia.modelo;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.StringProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Estudiante {
 
