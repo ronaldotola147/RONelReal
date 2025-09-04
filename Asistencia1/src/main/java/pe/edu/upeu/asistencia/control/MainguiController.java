7905687cc2f3fdee7e4620cf569edd003c281412
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-09-04 08:44:38 -0500

    CLASE

--- Asistencia1/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
+++ Asistencia1/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
@@ -1,9 +1,84 @@
 package pe.edu.upeu.asistencia.control;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 @Controller
 public class MainguiController {
+
+    @FXML
+    private MenuItem menuItem1,menuItem2;
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private TabPane tabPane;
+
+    @Autowired
+    private ApplicationContext context;
+
+
+    @FXML
+    public void initialize() {
+
+
+    }
+    class MenuItemListener  {
+
+
+
+
+        public void handle(ActionEvent e) {
+
+        }
+
+        public void abrirArchivoFXML(String fileName, String tittle) {
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(fileName));
+                loader.setControllerFactory(context::getBean);
+                Parent root = loader.load();
+
+                ScrollPane scrollPane = (ScrollPane) root;
+                scrollPane.setFitToWidth(true);
+                scrollPane.setFitToHeight(true);
+                Tab newTab = new Tab(tittle, scrollPane);
+                tabPane.getTabs().clear();
+                tabPane.getTabs().add(newTab);
+            }catch (IOException ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+    class MenuListener  {
+        public void handle(Event
+                                   e) {
+
+        }
+    }
 
 }
