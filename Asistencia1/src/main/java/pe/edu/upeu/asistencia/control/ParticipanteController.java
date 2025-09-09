2fed9a779dbe50aebe496032122a31faf60a6567
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-09-09 08:28:36 -0500

    AAAA

--- Asistencia1/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
+++ Asistencia1/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
@@ -1,10 +1,15 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import org.springframework.stereotype.Controller;
 import pe.edu.upeu.asistencia.enums.Carrera;
 import pe.edu.upeu.asistencia.enums.TipoParticipante;
+import pe.edu.upeu.asistencia.modelo.Participante;
 
 @Controller
 public class ParticipanteController {
@@ -15,10 +20,34 @@ public class ParticipanteController {
     @FXML
     private ComboBox<TipoParticipante> cbxTipoParticipante;
 
+
+    @FXML
+    TableView<Participante> tableView;
+    ObservableList<Participante> participantes;
+    TableColumn<Participante, String> dniCol,nombreCol,apellidoCol,carreraCol,tipoPartCol;
+
     @FXML
     public void initialize(){
         cbxCarrera.getItems().setAll(Carrera.values());
         cbxTipoParticipante.getItems().setAll(TipoParticipante.values());
 
     }
-}
+
+    public void definirNombresColumnas(){
+        dniCol = new TableColumn("DNI");
+        nombreCol = new TableColumn("NOMBRE");
+        apellidoCol = new TableColumn("APELLIDO");
+        apellidoCol.setMinWidth(200);
+        carreraCol = new TableColumn("Carrera");
+        tipoPartCol = new TableColumn("Tipo Participante");
+        tableView.getColumns().addAll(dniCol,nombreCol,apellidoCol,carreraCol,tipoPartCol);
+        }
+    }
+
+
+
+
+
+
+
+
